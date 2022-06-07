219dbb40e9a3f0661a3f7c630e84ac2e7695d2c3
anishazahan <anishazahan13@gmail.com>
2022-06-07 21:16:12 +0600

    banner-style-add

--- src/Components/Pages/Home/Banner.js
+++ src/Components/Pages/Home/Banner.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Banner.css'
 
 const Banner = () => {
   return (
     <div className='banner'>
       <div className="flex-item">
-            <h1 className='baner-heading text-5xl'>Hey !! I' am Anisha Zahan</h1>
+            <h1 className='baner-heading text-7xl'>Hey !! </h1>
+            <h1 className='text-6xl banner-heading2'>I'm <span className='name'>Anisha Zahan</span> </h1>
+            
+            <p className='banner-text'>I am a React JS developer,Web designer.I will create Your website as your creative choice! </p>
+            <Link className='banner-btn' to="/">Contact Me</Link>
+            <Link className='banner-btn ml-4' to="/">See Resume</Link>
       </div>
     </div>
   )
