8250edcd31f962296dc96f580d1eca48022ca012
anishazahan <anishazahan13@gmail.com>
2022-06-07 20:36:46 +0600

    banner-section

---
+++ src/Components/Pages/Home/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Banner.css'
+
+const Banner = () => {
+  return (
+    <div className='banner'>
+      <div className="flex-item">
+            <h1 className='baner-heading text-5xl'>Hey !! I' am Anisha Zahan</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
