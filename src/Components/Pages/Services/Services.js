707f87dce723a3f99787bf586ce91178dd1b20b3
anishazahan <anishazahan13@gmail.com>
2022-06-10 19:40:47 +0600

    service card heading

--- src/Components/Pages/Services/Services.js
+++ src/Components/Pages/Services/Services.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Services.css'
 import img from '../../../assets/img/Typing-bro.png'
+import responsiveImg from '../../../assets/img/responsive.svg'
 
 const Services = () => {
   return (
@@ -8,37 +9,37 @@ const Services = () => {
         <div className="service text-center mb-10">
         <h2 className=' servicing-heading text-3xl p-7'>My Services</h2>
         </div>
-        <p className=' text-center mb-7'>You will get 100% full service of work assured until you are fully satisfied</p>
+        <p className=' text-center mb-9'>You will get 100% full service of work assured until you are fully satisfied</p>
 
-        <div className="grid sm:grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        <div className="grid sm:grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5 gap-x-4">
+        <div className="box shadow-md px-4 pb-5 text-center">
+                <img className='img-design' src={responsiveImg} alt="" />
+                <h2 className='text-2xl service-name'>Responsive design</h2>
+                <p className='service-text'>I will make all responsive designs. I know all clients love responsive design. my all work Responsive Design. As a developer we need to complete Responsive design.  </p>
+            </div>
         <div className="box shadow-md px-4 pb-5 text-center">
                 <img className='img-design' src={img} alt="" />
-                <h2 className='text-xl'>Web design</h2>
+                <h2 className='text-2xl service-name'>Web design</h2>
+                <p className='service-text'>I am working with web design in the last 2 years. Now I have so much experience and I have completed so much design. I have so many clients to work with web design. </p>
+            </div>
+        <div className="box shadow-md px-4 pb-5 text-center">
+                <img className='img-design' src={img} alt="" />
+                <h2 className='text-xl service-name'>Web design</h2>
                 <p className='service-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In possimus eligendi cum laborum blanditiis nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, ut. </p>
             </div>
         <div className="box shadow-md px-4 pb-5 text-center">
                 <img className='img-design' src={img} alt="" />
-                <h2 className='text-xl'>Web design</h2>
-                <p className='service-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In possimus eligendi cum laborum blanditiis nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, ut. </p>
-            </div>
-        <div className="box shadow-md px-4 pb-5 text-center">
-                <img className='img-design' src={img} alt="" />
-                <h2 className='text-xl'>Web design</h2>
-                <p className='service-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In possimus eligendi cum laborum blanditiis nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, ut. </p>
-            </div>
-        <div className="box shadow-md px-4 pb-5 text-center">
-                <img className='img-design' src={img} alt="" />
-                <h2 className='text-xl'>Web design</h2>
+                <h2 className='text-xl service-name'>Web design</h2>
                 <p className='service-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In possimus eligendi cum laborum blanditiis nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, ut. </p>
             </div>
             <div className="box shadow-md px-4 pb-5 text-center">
                 <img className='img-design' src={img} alt="" />
-                <h2 className='text-xl'>Web design</h2>
+                <h2 className='text-xl service-name'>Web design</h2>
                 <p className='service-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In possimus eligendi cum laborum blanditiis nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, ut. </p>
             </div>
             <div className="box shadow-md px-4 pb-5 text-center">
                 <img className='img-design' src={img} alt="" />
-                <h2 className='text-xl'>Web design</h2>
+                <h2 className='text-xl service-name'>Web design</h2>
                 <p className='service-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In possimus eligendi cum laborum blanditiis nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, ut. </p>
             </div>
         </div>
