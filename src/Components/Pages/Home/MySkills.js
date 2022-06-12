8527cece97738546c9d64ac89efe92fa94b6fda3
anishazahan <anishazahan13@gmail.com>
2022-06-12 08:14:10 +0600

    skilss add

--- src/Components/Pages/Home/MySkills.js
+++ src/Components/Pages/Home/MySkills.js
@@ -14,7 +14,7 @@ const MySkills = () => {
             src={skillImg}
            alt=""/>
           <div>
-            <h1 className="text-3xl font-bold skill-name">Skils</h1>
+            <h1 className="text-xl font-bold skill-name mt-[-8] ">Skils</h1>
             <button className="skill-btn p-2">HTML</button>
             <button className="skill-btn p-2">CSS</button>
             <button className="skill-btn p-2">BOOTSTRAP</button>
@@ -22,6 +22,23 @@ const MySkills = () => {
             <button className="skill-btn p-2">DAYSIUI</button>
             <button className="skill-btn p-2">JAVASCRIPT</button>
             <button className="skill-btn p-2">REACT.JS</button>
+
+            <h1 className="text-xl font-bold skill-name mt-4">Familer</h1>
+            <button className="skill-btn p-2">Node.JS</button>
+            <button className="skill-btn p-2">EXpress.Js</button>
+            <button className="skill-btn p-2">Firebase Authentication</button>
+            <button className="skill-btn p-2">MongoDB</button><br/>
+            
+            <h1 className="text-xl font-bold skill-name mt-4">Deploy and Tools</h1>
+            <button className="skill-btn p-2">Git</button>
+            <button className="skill-btn p-2">GitHUB</button>
+            <button className="skill-btn p-2">Netlify</button>
+            <button className="skill-btn p-2">Firebase</button>
+            <button className="skill-btn p-2">Heroku</button><br/>
+            <button className="skill-btn p-2">VS Code</button>
+            <button className="skill-btn p-2">Figma</button>
+            <button className="skill-btn p-2">PhotoShop</button>
+            <button className="skill-btn p-2">Postman</button>
 
             
            
