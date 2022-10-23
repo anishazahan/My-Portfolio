77576b4428c4ae2084e33aff14566bd725a4a595
anishazahan <anishazahan13@gmail.com>
2022-10-23 19:06:58 +0600

    hoisting update

--- src/Components/Pages/Projects/Projects.js
+++ src/Components/Pages/Projects/Projects.js
@@ -89,7 +89,7 @@ const Projects = () => {
           </div>
           <h1 className='text-[18px] font-bold mt-4 mb-2 px-4 project-heading'>Best Choice</h1>
           <div className="flex flex-col space-y-1 pb-5 px-4">
-            <a className='text-sm link text-secondary' href="https://best-choice-a3d8e.web.app/products">Live Link</a>
+            <a className='text-sm link text-secondary' href="https://best-choice1-a6c56.web.app/">Live Link</a>
             <a className='text-sm link' href="https://github.com/anishazahan/Best-Choice">Client Code Link</a>
           </div>
         </div>
